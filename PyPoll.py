ddad9b1477e5cd75da291f9f504d86bcdab49a66
suyinwb <wong.suyin@gmail.com>
2021-11-16 23:10:32 -0500

    read & write initially

--- PyPoll.py
+++ PyPoll.py
@@ -19,26 +19,37 @@ print("The time right now is ", now)
 # Assign a variable for the file to load and the path.
 #file_to_load = 'resources/election_results.csv'
 file_to_load = os.path.join("resources", "election_results.csv")
+# Create a filename variable to a direct or indirect path to the file.
+file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Open the election results and read the file.
 #election_data = open(file_to_load, 'r')
-#with open(file_to_load, 'r') as election_data:
-#    for each in election_data:
-#        print(each)
+with open(file_to_load) as election_data:
+
+# Read the file object with the reader function.
+    file_reader = csv.reader(election_data)
+
+# Print the header row.
+    headers = next(file_reader)
+    #print(headers)
+
+# Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
 
 # To do: perform analysis.
 #for each_vote in election_data:
 #    print (each_vote)
 
 
-# Create a filename variable to a direct or indirect path to the file.
-file_to_save = os.path.join("analysis", "election_analysis.txt")
 
 # Using the with statement open the file as a text file.
 with open(file_to_save, "w") as txt_file:
 
     # Write some data to the file.
-    txt_file.write("Hello World")
+    txt_file.write("Counties in the Election\n")
+    txt_file.write("------------------------\n")
+    txt_file.write("Arapahoe\nDenver\nJefferson")
 
 # Close the file.
 election_data.close()
